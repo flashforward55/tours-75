9f4253aa36199eb348566d0ab3e996782de4f322
Anton Styazhkin <rudana089@gmail.com>
2023-06-22 22:55:03 +0300

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,28 +7,30 @@ import { DARK, LIGHT } from 'constans';
 // Vanila css
 // import './App.css';
 
-import style from './App.module.css';
+//import style from './App.module.css';
 import { Component } from 'react';
 
 class App extends Component {
-	state = {
-		theme: LIGHT,
-	};
+  state = {
+    theme: LIGHT,
+  };
 
-	handleChangeTheme = () => {
-		this.setState((prevState) => ({ theme: prevState.theme === DARK ? LIGHT : DARK }));
-	};
+  handleChangeTheme = () => {
+    this.setState(prevState => ({
+      theme: prevState.theme === DARK ? LIGHT : DARK,
+    }));
+  };
 
-	render() {
-		const { theme } = this.state;
-		return (
-			<div className='app-container'>
-				<Header theme={theme} onChangeTheme={this.handleChangeTheme} />
-				<Tours theme={theme} />
-				<Footer />
-			</div>
-		);
-	}
+  render() {
+    const { theme } = this.state;
+    return (
+      <div className="app-container">
+        <Header theme={theme} onChangeTheme={this.handleChangeTheme} />
+        <Tours theme={theme} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
