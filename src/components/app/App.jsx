476e5a7cefa67e18b4b3636e7163a941a96c8937
Anton Styazhkin <rudana089@gmail.com>
2023-06-24 23:16:20 +0300

    i-tours-75-07-hooks

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 import Header from 'components/header';
-import Tours from 'components/tours';
+//import Tours from 'components/tours';
 import Footer from 'components/footer';
 
 import { DARK, LIGHT } from 'constans';
@@ -7,35 +7,37 @@ import { DARK, LIGHT } from 'constans';
 // Vanila css
 // import './App.css';
 
-import style from './App.module.css';
+//import style from './App.module.css';
 import { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ToursHooks from 'components/tours/ToursHooks';
 
 class App extends Component {
-	state = {
-		theme: LIGHT,
-	};
+  state = {
+    theme: LIGHT,
+  };
 
-	handleChangeTheme = () => {
-		this.setState((prevState) => ({ theme: prevState.theme === DARK ? LIGHT : DARK }));
-	};
+  handleChangeTheme = () => {
+    this.setState(prevState => ({
+      theme: prevState.theme === DARK ? LIGHT : DARK,
+    }));
+  };
 
-	render() {
-		const { theme } = this.state;
-		return (
-			<div className='app-container'>
-				<Header theme={theme} onChangeTheme={this.handleChangeTheme} />
+  render() {
+    const { theme } = this.state;
+    return (
+      <div className="app-container">
+        <Header theme={theme} onChangeTheme={this.handleChangeTheme} />
 
-				<Routes>
-					<Route path='/' element={<ToursHooks theme={theme} />} />
-					<Route path='/foo' element={<></>} />
-				</Routes>
+        <Routes>
+          <Route path="/" element={<ToursHooks theme={theme} />} />
+          <Route path="/foo" element={<></>} />
+        </Routes>
 
-				<Footer />
-			</div>
-		);
-	}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,29 @@
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = ({ theme, onChangeTheme }) => {
-	// componentDidMount
+  // componentDidMount
 
-	// useEffect(() => {
-	// 	console.log('work useEffect');
-	// }, []);
+  // useEffect(() => {
+  // 	console.log('work useEffect');
+  // }, []);
 
-	// console.log('render', toggle);
+  // console.log('render', toggle);
 
-	return (
-		<header
-			style={{
-				display: 'flex',
-				justifyContent: 'space-between',
-				alignItems: 'center',
-			}}>
-			<p>logo</p>
-			<Link to='/foo'>go to</Link>
+  return (
+    <header
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}>
+      <p>logo</p>
+      <Link to='/foo'>go to</Link>
 
-			<button onClick={onChangeTheme}>Current Theme:{theme}</button>
-		</header>
-	);
+      <button onClick={onChangeTheme}>Current Theme:{theme}</button>
+    </header>
+  );
 };
 
 export default Header;
