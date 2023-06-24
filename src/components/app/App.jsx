bda7cea2b25d4f911823db1947288439403f473b
Anton Styazhkin <rudana089@gmail.com>
2023-06-24 23:01:43 +0300

    i-tours-75-05-lifecycle-component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,30 +7,34 @@ import { DARK, LIGHT } from 'constans';
 // Vanila css
 // import './App.css';
 
-//import style from './App.module.css';
+import style from './App.module.css';
 import { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 class App extends Component {
-  state = {
-    theme: LIGHT,
-  };
+	state = {
+		theme: LIGHT,
+	};
 
-  handleChangeTheme = () => {
-    this.setState(prevState => ({
-      theme: prevState.theme === DARK ? LIGHT : DARK,
-    }));
-  };
+	handleChangeTheme = () => {
+		this.setState((prevState) => ({ theme: prevState.theme === DARK ? LIGHT : DARK }));
+	};
 
-  render() {
-    const { theme } = this.state;
-    return (
-      <div className="app-container">
-        <Header theme={theme} onChangeTheme={this.handleChangeTheme} />
-        <Tours theme={theme} />
-        <Footer />
-      </div>
-    );
-  }
+	render() {
+		const { theme } = this.state;
+		return (
+			<div className='app-container'>
+				<Header theme={theme} onChangeTheme={this.handleChangeTheme} />
+
+				<Routes>
+					<Route path='/' element={<Tours theme={theme} />} />
+					<Route path='/foo' element={<></>} />
+				</Routes>
+
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
