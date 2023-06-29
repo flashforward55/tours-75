acc9238f6b66e25db17b31daa2739d827de457f1
Anton Styazhkin <rudana089@gmail.com>
2023-06-29 13:14:39 +0300

    Fix files

--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import TourFormModal from 'components/tour-form-modal/TourFormModal';
 import ToursItem from 'components/tours-item/ToursItems';
 import debounce from 'lodash.debounce';
@@ -6,117 +6,117 @@ import { addTour, fetchTours } from 'api/tours';
 import useToggle from 'hooks/useToggle';
 
 const ToursHooks = () => {
-	const { visible, open, close } = useToggle(false);
+  const { visible, open, close } = useToggle(false);
 
-	const [isLoading, setIsLoading] = useState(false);
+  const [isLoading] = useState(false);
 
-	const [query, setQuery] = useState('');
-	const [errorMessage, setError] = useState('');
+  const [query, setQuery] = useState('');
+  const [errorMessage] = useState('');
 
-	const [toursData, setToursData] = useState({
-		total_items: 0,
-		items: [],
-	});
+  const [toursData, setToursData] = useState({
+    total_items: 0,
+    items: [],
+  });
 
-	// Prevent request on first render
+  // Prevent request on first render
 
-	// const isFirstRender = useRef(true);
+  // const isFirstRender = useRef(true);
 
-	// useEffect(() => {
-	// 	if (isFirstRender.current) {
-	// 		isFirstRender.current = false;
-	// 		return;
-	// 	}
+  // useEffect(() => {
+  // 	if (isFirstRender.current) {
+  // 		isFirstRender.current = false;
+  // 		return;
+  // 	}
 
-	// 	const loadTours = async () => {
-	// 		const data = await fetchTours(query);
-	// 		setToursData(data);
-	// 	};
+  // 	const loadTours = async () => {
+  // 		const data = await fetchTours(query);
+  // 		setToursData(data);
+  // 	};
 
-	// 	loadTours();
-	// }, [query]);
+  // 	loadTours();
+  // }, [query]);
 
-	// const loadTours = useCallback(async () => {
-	// 	const data = await fetchTours(query);
-	// 	setToursData(data);
-	// }, [query]);
+  // const loadTours = useCallback(async () => {
+  // 	const data = await fetchTours(query);
+  // 	setToursData(data);
+  // }, [query]);
 
-	// componentDidMount + componentDidUpdate
+  // componentDidMount + componentDidUpdate
 
-	useEffect(() => {
-		const load = async () => {
-			const data = await fetchTours(query);
-			setToursData(data);
-		};
-		load();
-	}, []);
+  useEffect(() => {
+    const load = async () => {
+      const data = await fetchTours(query);
+      setToursData(data);
+    };
+    load();
+  }, [query]);
 
-	// componentWillUnmount
+  // componentWillUnmount
 
-	useEffect(() => () => console.log('componentWillUnmount'), []);
+  useEffect(() => () => console.log('componentWillUnmount'), []);
 
-	const handleChangeQuery = debounce((e) => {
-		setQuery(e.target.value);
-	}, 1000);
+  const handleChangeQuery = debounce((e) => {
+    setQuery(e.target.value);
+  }, 1000);
 
-	const handleAddNewTour = async (tour) => {
-		await addTour(tour);
-		// loadTours();
-	};
+  const handleAddNewTour = async (tour) => {
+    await addTour(tour);
+    // loadTours();
+  };
 
-	const filteredItemsCached = useMemo(
-		() =>
-			toursData.items.filter((el) => {
-				return el.name.toLowerCase().includes(query.toLowerCase());
-			}),
-		[toursData.items, query]
-	);
+  const filteredItemsCached = useMemo(
+    () =>
+      toursData.items.filter((el) => {
+        return el.name.toLowerCase().includes(query.toLowerCase());
+      }),
+    [toursData.items, query]
+  );
 
-	return (
-		<>
-			<TourFormModal visible={visible} onClose={close} onAddTour={handleAddNewTour} />
+  return (
+    <>
+      <TourFormModal visible={visible} onClose={close} onAddTour={handleAddNewTour} />
 
-			<div className='tours-container'>
-				<div className='tours-container__controlls'>
-					<h1>Tours page</h1>
-					<input
-						id='search-input'
-						type='text'
-						placeholder='search...'
-						onChange={handleChangeQuery}
-					/>
-					<button onClick={open}>Add tour</button>
-				</div>
+      <div className='tours-container'>
+        <div className='tours-container__controlls'>
+          <h1>Tours page</h1>
+          <input
+            id='search-input'
+            type='text'
+            placeholder='search...'
+            onChange={handleChangeQuery}
+          />
+          <button onClick={open}>Add tour</button>
+        </div>
 
-				{isLoading ? (
-					<div>loading...</div>
-				) : (
-					<>
-						{errorMessage ? (
-							<p>{errorMessage}</p>
-						) : (
-							<>
-								{/* {!!toursData.total_items && ( */}
-								<div>
-									<p>Total items:{toursData.total_items}</p>
-									<ul className='tours-list'>
-										{filteredItemsCached.map((tour) => (
-											<ToursItem
-												{...tour}
-												key={tour.id}
-												// onDelete={this.handleRemoveTour}
-											/>
-										))}
-									</ul>
-								</div>
-								{/* )} */}
-							</>
-						)}
-					</>
-				)}
-			</div>
-		</>
-	);
+        {isLoading ? (
+          <div>loading...</div>
+        ) : (
+          <>
+            {errorMessage ? (
+              <p>{errorMessage}</p>
+            ) : (
+              <>
+                {/* {!!toursData.total_items && ( */}
+                <div>
+                  <p>Total items:{toursData.total_items}</p>
+                  <ul className='tours-list'>
+                    {filteredItemsCached.map((tour) => (
+                      <ToursItem
+                        {...tour}
+                        key={tour.id}
+                      // onDelete={this.handleRemoveTour}
+                      />
+                    ))}
+                  </ul>
+                </div>
+                {/* )} */}
+              </>
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ToursHooks;
